c973ff521ef4f3dbf507d62c916462585354df19
SimonMueller <SimonMueller@users.noreply.github.com>
2020-03-29 19:33:17 +0000

    Deploying to gh-pages from  - 7616eed54ac697cec33ae9957d1d0b2b37b9daea 🚀

--- precache-manifest.b09a9b37ce12a7a15e719c390dd4ff4c.js
+++ precache-manifest.b09a9b37ce12a7a15e719c390dd4ff4c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6e10f39636caa5792d5bccd7b1dc1aeb",
+    "revision": "765b453e0db112b8228d4e0fdea8813b",
     "url": "/agurk-client/index.html"
   },
   {
-    "revision": "a14296e535d353748b12",
+    "revision": "12786fb19298fe3b2fd5",
     "url": "/agurk-client/static/css/main.039a6f61.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/agurk-client/static/js/2.09fca9ef.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a14296e535d353748b12",
-    "url": "/agurk-client/static/js/main.1356d1b4.chunk.js"
+    "revision": "12786fb19298fe3b2fd5",
+    "url": "/agurk-client/static/js/main.2a92d56d.chunk.js"
   },
   {
     "revision": "6df46cf3a0ecd0088cd2",
