52dc0dbb96ce390c34e030aaf1092e196ecd1565
Simon Müller <SimonMueller@users.noreply.github.com>
2020-04-13 23:01:27 +0200

    overlap hand for small devices

--- src/components/board/SelectableCardList.tsx
+++ src/components/board/SelectableCardList.tsx
@@ -3,6 +3,7 @@ import { Card as CardData } from 'agurk-shared';
 import styled from 'styled-components';
 import PlayingCard, { generateCardKey } from './PlayingCard';
 import { UnstyledButton } from '../styled/Button';
+import { Theme } from '../styled/theme';
 
 export type SelectableCard = CardData & { isSelected: boolean };
 
@@ -11,15 +12,26 @@ interface Props {
   handleSelect: (card: CardData) => void;
 }
 
+const cardBoxOverlap = '3em';
+
 const Flex = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin-bottom: 1em;
+  width: calc(100% - ${cardBoxOverlap});
+
+  @media(min-width: ${({ theme }: { theme: Theme }) => (theme.breakpoints.minWidth)}) {
+    width: 100%;
+  }
 `;
 
 const Box = styled.div`
-  margin-right: 0.5em;
+  margin-right: -${cardBoxOverlap};
   margin-bottom: 0.5em;
+
+  @media(min-width: ${({ theme }: { theme: Theme }) => (theme.breakpoints.minWidth)}) {
+    margin-right: 0.5em;
+  }
 `;
 
 export default function CardList({ cards, handleSelect }: Props) {
