54037cff87da27b77e9e66d4283f87e43179b801
Simon Müller <SimonMueller@users.noreply.github.com>
2020-04-15 20:29:38 +0200

    smaller components for player lists

--- src/components/board/PlayerStateList.tsx
+++ src/components/board/PlayerStateList.tsx
@@ -15,26 +15,39 @@ function byOrderAsc(first: PlayerState, second: PlayerState) {
   return Math.sign(first.order - second.order);
 }
 
-export default function PlayerStateList({ players }: Props) {
+function ActiveOrderedPlayers({ players }: Props) {
   const orderedActivePlayers = players.filter((player) => !player.isOut).sort(byOrderAsc);
-  const outPlayers = players.filter((player) => player.isOut);
   const activePlayerItems = orderedActivePlayers.map((player) => <Li key={player.id}><Player player={player} /></Li>);
+
+  return (
+    <ol>
+      { activePlayerItems }
+    </ol>
+  );
+}
+
+function OutPlayers({ players }: Props) {
+  const outPlayers = players.filter((player) => player.isOut);
   const outPlayerItems = outPlayers.map((player) => <Li key={player.id}><Player player={player} /></Li>);
 
+  if (outPlayerItems.length === 0) {
+    return null;
+  }
+
+  return (
+    <ul>
+      { outPlayerItems }
+    </ul>
+  );
+}
+
+export default function PlayerStateList({ players }: Props) {
   return (
     <div>
       <h2>Players</h2>
 
-      <ol>
-        { activePlayerItems }
-      </ol>
-
-      { outPlayerItems.length > 0
-        && (
-        <ul>
-          { outPlayerItems }
-        </ul>
-        )}
+      <ActiveOrderedPlayers players={players} />
+      <OutPlayers players={players} />
     </div>
   );
 }
