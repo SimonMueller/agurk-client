2910c4ecf3cb5136e84196296228d9282c905015
Simon Müller <SimonMueller@users.noreply.github.com>
2020-04-26 14:08:50 +0200

    add component tests for text second timer

---
+++ src/components/TextSecondTimer.test.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { render, act } from '../utils/test-utils';
+import TextSecondTimer from './TextSecondTimer';
+
+describe('Text second timer component', () => {
+  beforeEach(() => jest.useFakeTimers());
+
+  it('shows zero if timeout is zero', async () => {
+    const { getByText } = render(<TextSecondTimer timeoutInSeconds={0} />);
+
+    expect(getByText('0')).toBeVisible();
+  });
+
+  it('shows initial timeout value if timeout is below zero', () => {
+    const { getByText } = render(<TextSecondTimer timeoutInSeconds={-5} />);
+
+    expect(getByText('-5')).toBeVisible();
+  });
+
+  it('shows initial timeout value if timeout is greater than zero', () => {
+    const { getByText } = render(<TextSecondTimer timeoutInSeconds={20} />);
+
+    expect(getByText('20')).toBeVisible();
+  });
+
+  it('shows timeout value minus advanced time', () => {
+    const { getByText } = render(<TextSecondTimer timeoutInSeconds={20} />);
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(getByText('10')).toBeVisible();
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(getByText('0')).toBeVisible();
+  });
+
+  it('shows zero after all timers have run', () => {
+    const { getByText } = render(<TextSecondTimer timeoutInSeconds={20} />);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(getByText('0')).toBeVisible();
+  });
+});
