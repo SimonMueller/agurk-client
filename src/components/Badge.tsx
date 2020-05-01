d3956f823b1353581fe8a9af71037f45d4c1eee0
Simon Müller <SimonMueller@users.noreply.github.com>
2020-05-01 23:16:36 +0200

    badge display inline block

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -5,9 +5,9 @@ export default styled.span`
   background-color: ${({ theme, severity }: { theme: Theme, severity: Severity }) => (theme.palette[severity].main)};
   border-radius: 0.25rem;
   font-size: ${({ theme }: { theme: Theme }) => (theme.typography.caption.fontSize)};
-  margin: 0 ${({ theme }: { theme: Theme }) => (theme.spacing(1))}px;
   padding: 0.15rem 0.25rem;
   font-weight: bold;
   text-transform: uppercase;
   color: white;
+  display: inline-block;
 `;
